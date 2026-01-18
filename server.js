8000646877d51c04da26b33c92488b2b04c98d77
Tyler Cortez <tylercortez777@gmail.com>
2026-01-18 12:54:59 +0400

    57th

--- server.js
+++ server.js
@@ -23,14 +23,21 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
+app.set("trust proxy", 1);
+
 //Session setup for login persistance
 app.use(
   session({
-    secret: "your_secret_key",
+    secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    cookie: {
+      secure: true,
+      sameSite: "lax"
+    }
   })
 );
+
 //The one that initiate the passport for the Google Login
 app.use(passport.initialize());
 app.use(passport.session());
